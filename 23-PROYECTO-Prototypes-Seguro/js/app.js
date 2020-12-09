29e5a0199df82ca3269c9258d9f6d5899ad85239
Javiuty <javiiescuadra@gmail.com>
2020-12-09 19:53:27 +0100

    validando formulario

--- 23-PROYECTO-Prototypes-Seguro/js/app.js
+++ 23-PROYECTO-Prototypes-Seguro/js/app.js
@@ -22,6 +22,28 @@ UI.prototype.llenarOpciones = () => {
   }
 };
 
+//  Muestra alertas en pantalla
+UI.prototype.mostrarMensaje = (mensaje, tipo) => {
+  const div = document.createElement("div");
+
+  if (tipo === "error") {
+    div.classList.add("error");
+  } else {
+    div.classList.add("correcto");
+  }
+
+  div.classList.add("mensaje", "mt-10");
+  div.textContent = mensaje;
+
+  // Insertar en HTML
+  const formulario = document.querySelector("#cotizar-seguro");
+  formulario.insertBefore(div, document.querySelector("#resultado"));
+
+  setTimeout(() => {
+    div.remove();
+  }, 3000);
+};
+
 // Instanciar UI
 const ui = new UI();
 console.log(ui);
@@ -29,3 +51,34 @@ console.log(ui);
 document.addEventListener("DOMContentLoaded", () => {
   ui.llenarOpciones(); // LLena el select con los años
 });
+
+eventListeners();
+function eventListeners() {
+  const formulario = document.querySelector("#cotizar-seguro");
+
+  formulario.addEventListener("submit", cotizarSeguro);
+}
+
+function cotizarSeguro(e) {
+  e.preventDefault();
+
+  // Leer la marca seleccionada
+  const marca = document.querySelector("#marca").value;
+
+  // Leer el año seleccionado
+  const year = document.querySelector("#year").value;
+
+  // Leer el tipo de cobertura
+  const tipo = document.querySelector('input[name="tipo"]:checked').value;
+
+  if (marca === "" || year === "" || tipo === "") {
+    ui.mostrarMensaje("Todos los campos son obligatorios", "error");
+    return;
+  }
+
+  ui.mostrarMensaje("Cotizando...", "exito");
+
+  // Instanciar el seguro
+
+  // Utilizar el prototype que va a cotizar
+}
